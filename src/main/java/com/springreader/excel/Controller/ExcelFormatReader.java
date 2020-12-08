51de079a29d9de9ed364ed34414cac30c9d957f3
ntwari-egide <ntwariegide2@gmail.com>
2020-12-08 21:08:34 +0200

    Handling the post of creating the files

--- src/main/java/com/springreader/excel/Controller/ExcelFormatReader.java
+++ src/main/java/com/springreader/excel/Controller/ExcelFormatReader.java
@@ -4,11 +4,12 @@ import com.springreader.excel.Model.Member;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -85,5 +86,70 @@ public class ExcelFormatReader {
         }
 
         return members;
+    }
+
+    @PostMapping("/createExcel")
+    public ResponseEntity<?> createExcelFile(@RequestBody List<Member> members)throws Exception{
+        String[] columns = {"member no", "first name", "second name", "marks","position"};
+
+        // Create a Workbook
+        Workbook workbook = new XSSFWorkbook(); // new HSSFWorkbook() for generating `.xls` file
+
+        /* CreationHelper helps us create instances of various things like DataFormat,
+           Hyperlink, RichTextString etc, in a format (HSSF, XSSF) independent way */
+        CreationHelper createHelper = workbook.getCreationHelper();
+
+        // Create a Sheet
+        Sheet sheet = workbook.createSheet("Employee");
+
+        // Create a Font for styling header cells
+        Font headerFont = workbook.createFont();
+        headerFont.setBold(true);
+        headerFont.setFontHeightInPoints((short) 14);
+        headerFont.setColor(IndexedColors.RED.getIndex());
+
+        // Create a CellStyle with the font
+        CellStyle headerCellStyle = workbook.createCellStyle();
+        headerCellStyle.setFont(headerFont);
+
+        // Create a Row
+        Row headerRow = sheet.createRow(0);
+
+        // Create cells
+        for(int i = 0; i < columns.length; i++) {
+            Cell cell = headerRow.createCell(i);
+            cell.setCellValue(columns[i]);
+            cell.setCellStyle(headerCellStyle);
+        }
+
+        int rowNum = 1;
+        for (Member member: members){
+            Row row = sheet.createRow(rowNum++);
+            row.createCell(1)
+                    .setCellValue(member.getMemberNo());
+            row.createCell(2)
+                    .setCellValue(member.getFirstName());
+            row.createCell(3)
+                    .setCellValue(member.getSecondName());
+            row.createCell(4)
+                    .setCellValue(member.getMarks());
+            row.createCell(5)
+                    .setCellValue(member.getPosition());
+        }
+
+        // Resize all columns to fit the content size
+        for(int i = 0; i < columns.length; i++) {
+            sheet.autoSizeColumn(i);
+        }
+
+        // Write the output to a file
+        FileOutputStream fileOut = new FileOutputStream("created-excel-file.xlsx");
+        workbook.write(fileOut);
+        fileOut.close();
+
+        // Closing the workbook
+        workbook.close();
+
+        return ResponseEntity.ok().build();
     }
 }
