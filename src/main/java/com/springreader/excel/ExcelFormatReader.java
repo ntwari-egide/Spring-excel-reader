53c8492a633ea8bc62c464024dbfb1ce3b69b7af
ntwari-egide <ntwariegide2@gmail.com>
2020-12-08 18:28:32 +0200

    Understanding usage of excel reader

--- src/main/java/com/springreader/excel/ExcelFormatReader.java
+++ src/main/java/com/springreader/excel/ExcelFormatReader.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExcelFormatReader {
     @GetMapping("/excelReader1")
     public String simpleReadingFromExcel(){
+        //The Apache POI library supports both .xls and .xlsx files and is a more complex library than other Java libraries for working with Excel files.
+        //When working with the newer .xlsx file format, you would use the XSSFWorkbook, XSSFSheet, XSSFRow, and XSSFCell classes.
+        //To work with the older .xls format, use the HSSFWorkbook, HSSFSheet, HSSFRow, and HSSFCell classes.
+
+
         return "excel file is read";
     }
 }
