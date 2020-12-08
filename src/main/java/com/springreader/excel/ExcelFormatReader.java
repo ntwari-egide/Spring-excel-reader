f9c31888bc0a83442c9df765159a7d8966451d48
ntwari-egide <ntwariegide2@gmail.com>
2020-12-08 18:47:43 +0200

    Adding the sheet

--- src/main/java/com/springreader/excel/ExcelFormatReader.java
+++ src/main/java/com/springreader/excel/ExcelFormatReader.java
@@ -1,12 +1,17 @@
 package com.springreader.excel;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ExcelFormatReader {
@@ -18,7 +23,17 @@ public class ExcelFormatReader {
 
         File file = new File("C:\\Users\\user\\Documents\\ntwari egide documents\\spring boot\\Spring-excel-reader\\src\\main\\resources\\members.xlsx");
         FileInputStream fileInputStream = new FileInputStream(file);
-        Workbook cheet = new HSSFWorkbook(file);
+        Workbook workbook = new HSSFWorkbook(fileInputStream);
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+        // Generating Map to store the data from the cheet
+        Map<Integer , List<String>> data = new HashMap<>();
+        int i = 0;
+
+        for (Row row : cheet){
+
+        }
 
         return "excel file is read";
     }
