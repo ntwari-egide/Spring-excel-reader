a78dc1075f7f2345e8727d47d554aa242466a0b8
ntwari-egide <ntwariegide2@gmail.com>
2020-12-08 18:45:05 +0200

    Getting the file

--- src/main/java/com/springreader/excel/ExcelFormatReader.java
+++ src/main/java/com/springreader/excel/ExcelFormatReader.java
@@ -1,16 +1,24 @@
 package com.springreader.excel;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 @RestController
 public class ExcelFormatReader {
     @GetMapping("/excelReader1")
-    public String simpleReadingFromExcel(){
+    public String simpleReadingFromExcel()throws Exception{
         //The Apache POI library supports both .xls and .xlsx files and is a more complex library than other Java libraries for working with Excel files.
         //When working with the newer .xlsx file format, you would use the XSSFWorkbook, XSSFSheet, XSSFRow, and XSSFCell classes.
         //To work with the older .xls format, use the HSSFWorkbook, HSSFSheet, HSSFRow, and HSSFCell classes.
 
+        File file = new File("C:\\Users\\user\\Documents\\ntwari egide documents\\spring boot\\Spring-excel-reader\\src\\main\\resources\\members.xlsx");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        Workbook cheet = new HSSFWorkbook(file);
 
         return "excel file is read";
     }
